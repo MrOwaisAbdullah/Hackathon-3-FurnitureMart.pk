639023b451f58559fa3aeef8e0b0d6d9b9cd5eb1
MrOwaisAbdullah <mrowaisabdullah@gmail.com>
2025-02-05 02:54:57 +0500

    Updated

--- app/track/[trackingId]/page.tsx
+++ app/track/[trackingId]/page.tsx
@@ -1,14 +1,24 @@
-// import { useRouter } from "next/router";
-import TrackOrder from "@/components/sections/TrackOrder";
+// // import { useRouter } from "next/router";
+// import TrackOrder from "@/components/sections/TrackOrder";
 
-const TrackingPage = () => {
-  const { trackingId } = router.query;
+// const TrackingPage = () => {
+//   // const { trackingId } = router.query;
 
-  if (!trackingId) {
-    return <p>Invalid tracking ID.</p>;
-  }
+//   // if (!trackingId) {
+//   //   return <p>Invalid tracking ID.</p>;
+//   // }
 
-  return <TrackOrder trackingId={trackingId as string} />;
-};
+//   return <TrackOrder trackingId={trackingId as string} />;
+// };
 
-export default TrackingPage;
+// export default TrackingPage;
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
