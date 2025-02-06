a305eb8849a23624e17d941cbf9d0a505b0f78bf
MrOwaisAbdullah <mrowaisabdullah@gmail.com>
2025-02-06 11:17:22 +0500

    Updated

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -17,7 +17,6 @@ import { CiCircleAlert, CiHeart } from "react-icons/ci";
 import { useCart } from "@/app/context/CartContext";
 
 const Header = () => {
-
   const { state } = useCart(); // Access the cart state from context
   const { cart } = state;
 
@@ -26,7 +25,7 @@ const Header = () => {
     () => cart.reduce((total, item) => total + (item.quantity || 0), 0),
     [cart]
   );
-  
+
   // State to manage the open/close status of the drawer
   const [isOpen, setIsOpen] = useState(false);
 
@@ -62,95 +61,110 @@ const Header = () => {
       </div>
       {/* Middle Section */}
       <div
-        className={`flex  bg-secondary w-full text-text lg:justify-between px-5 lg:px-12 h-[5.5rem]`}
+        className={`flex gap-3 bg-secondary w-full text-text justify-between px-5 lg:px-12 h-[5.5rem]`}
       >
-        <div className=" flex max-w-7xl m-auto pt-5 pb-2 w-full flex-row justify-between items-center">
+        <div className=" flex max-w-7xl m-auto pt-5 pb-2 w-full justify-between items-center">
           <Link
             href="/"
             className={`flex text-2xl gap-2 font-medium items-center`}
           >
-            <Image src={logo} alt=""></Image>
-            Comforty
+            <Image src={logo} alt="FurnitureMart.pk"></Image>
+            FurnitureMart
           </Link>
           <div className="flex gap-5 items-center justify-center">
-            <Link href="/wishlist" className="hidden xs:flex items-center gap-2 hover:text-primary">
-            <CiHeart className="w-6 h-6 " />
+            <Link
+              href="/wishlist"
+              className="hidden md:flex items-center gap-2 hover:text-primary"
+            >
+              <CiHeart className="w-6 h-6 " />
             </Link>
-          <Link href={"/cart"}>
-            <div className="group bg-white py-3 px-5 rounded-xl gap-3 hidden xs:flex items-center justify-center max-w-40 ">
-              <PiShoppingCart className="text-2xl group-hover:text-primary flex items-center justify-center" />
-              <p className="group-hover:text-primary">Cart</p>
-              <div className="rounded-full text-xs min-w-5 min-h-5 flex justify-center items-center text-center text-white bg-primary">
-                {cartCount}
+            <Link href={"/cart"}>
+              <div className="group bg-white py-3 px-3 md:px-5 rounded-xl gap-3 hidden xs:flex items-center justify-center max-w-30 sm:max-w-40 ">
+                <PiShoppingCart className="text-2xl group-hover:text-primary flex items-center justify-center" />
+                <p className="group-hover:text-primary">Cart</p>
+                <div className="rounded-full text-xs min-w-5 min-h-5 flex justify-center items-center text-center text-white bg-primary">
+                  {cartCount}
+                </div>
               </div>
-            </div>
-          </Link>
-          {/* Mobile Menu with shadcn Component */}
-          <Drawer direction="left" open={isOpen} onOpenChange={setIsOpen}>
-            {/* Button to Toggle Drawer */}
-            <button onClick={toggleDrawer} className="text-3xl lg:hidden">
-              <CgMenuRight />
-            </button>
-            <DrawerContent className="bg-background mx-auto max-w-[80%] text-left px-6 lg:max-w-[35%] md:max-w-[50%]">
-              {/* Close Button */}
-              <DrawerClose
-                className="m-5 text-xl place-self-end"
-                onClick={closeDrawer}
-              >
-                <CgCloseO />
-              </DrawerClose>
-              <DrawerTitle className="text-xl my-5 font-bold">
-                Main Menu
-              </DrawerTitle>
-              {/* Each link includes an onClick handler to close the drawer */}
-              <div className="flex flex-col gap-5 text-2xl">
-                <Link
-                  href="/"
-                  className="mr-9 text-lg  text-[#636270] active:text-primary hover:text-primary"
+            </Link>
+            {/* Mobile Menu with shadcn Component */}
+            <Drawer direction="left" open={isOpen} onOpenChange={setIsOpen}>
+              {/* Button to Toggle Drawer */}
+              <button onClick={toggleDrawer} className="text-3xl lg:hidden">
+                <CgMenuRight />
+              </button>
+              <DrawerContent className="bg-background mx-auto max-w-[80%] text-left px-6 lg:max-w-[35%] md:max-w-[50%]">
+                {/* Close Button */}
+                <DrawerClose
+                  className="absolute top-3 right-3 m-5 text-xl"
                   onClick={closeDrawer}
                 >
-                  Home
-                </Link>
+                  <CgCloseO />
+                </DrawerClose>
+                <DrawerTitle className="text-xl my-5 font-bold">
+                  Main Menu
+                </DrawerTitle>
+                {/* Each link includes an onClick handler to close the drawer */}
+                <div className="flex flex-col gap-5 text-2xl">
+                  <Link
+                    href="/"
+                    className="mr-9 text-lg  text-[#636270] active:text-primary hover:text-primary"
+                    onClick={closeDrawer}
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    href="/shop"
+                    className="mr-9 text-lg text-[#636270] active:text-primary hover:text-primary"
+                    onClick={closeDrawer}
+                  >
+                    Shop
+                  </Link>
+                  <Link
+                    href="/contact"
+                    className="mr-9 text-lg text-[#636270] active:text-primary hover:text-primary"
+                    onClick={closeDrawer}
+                  >
+                    Contact
+                  </Link>
+
+                  <Link
+                    href="/about"
+                    className="mr-9 text-lg text-[#636270] active:text-primary hover:text-primary"
+                    onClick={closeDrawer}
+                  >
+                    About
+                  </Link>
+                </div>
                 <Link
-                  href="/shop"
-                  className="mr-9 text-lg text-[#636270] active:text-primary hover:text-primary"
+                  href="/wishlist"
+                  className="group flex items-center mt-2 gap-2 group-hover:text-primary"
                   onClick={closeDrawer}
                 >
-                  Shop
-                </Link>
-                <Link
-                  href="/contact"
-                  className="mr-9 text-lg text-[#636270] active:text-primary hover:text-primary"
-                  onClick={closeDrawer}
-                >
-                  Contact
+                  <span className="flex items-center gap-2 mr-9 text-lg text-[#636270] active:text-primary group-hover:text-primary">
+                    <CiHeart className="w-6 h-6 " />
+                    Wishlist
+                  </span>
                 </Link>
 
-                <Link
-                  href="/about"
-                  className="mr-9 text-lg text-[#636270] active:text-primary hover:text-primary"
-                  onClick={closeDrawer}
-                >
-                  About
+                <div className="items-center flex mt-5">
+                  <p className="text-base font-medium text-gray-500">
+                    Contact:
+                    <span className="ml-2 text-text">(808) 555-0111</span>
+                  </p>
+                </div>
+
+                <Link href={"/cart"} onClick={closeDrawer}>
+                  <div className="group mt-5 rounded-xl gap-3 flex items-center max-w-40 ">
+                    <PiShoppingCart className="text-3xl group-hover:text-primary flex items-center justify-center" />
+                    <p className="group-hover:text-primary">Cart</p>
+                    <div className="rounded-full text-xs min-w-5 min-h-5 flex justify-center items-center text-center text-white bg-primary">
+                      {cartCount}
+                    </div>
+                  </div>
                 </Link>
-              </div>
-              <div className="items-center flex mt-5">
-            <p className="text-base font-medium text-gray-500">
-              Contact:<span className="ml-2 text-text">(808) 555-0111</span>
-            </p>
-          </div>
-          <Link href={"/cart"} 
-          onClick={closeDrawer}>
-            <div className="group mt-5 rounded-xl gap-3 flex items-center max-w-40 ">
-              <PiShoppingCart className="text-3xl group-hover:text-primary flex items-center justify-center" />
-              <p className="group-hover:text-primary">Cart</p>
-              <div className="rounded-full text-xs min-w-5 min-h-5 flex justify-center items-center text-center text-white bg-primary">
-                {cartCount}
-              </div>
-            </div>
-          </Link>
-            </DrawerContent>
-          </Drawer>
+              </DrawerContent>
+            </Drawer>
           </div>
         </div>
       </div>
