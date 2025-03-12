94aab00904372c1707629fc852b591e83cf32c03
Owais Abdullah <mrowaisabdullah@gmail.com>
2025-03-13 01:39:04 +0500

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/sections/Header";
 import Footer from "@/components/sections/Footer";
-import { CartProvider } from "./context/CartContext";
-import { NotificationsProvider } from "./context/NotificationContext";
-import { ToastContainer } from "@/components/ui/Toast";
-import { WishlistProvider } from "./context/WishlistContext";
 import dynamic from "next/dynamic";
 
 export const metadata: Metadata = {
@@ -13,6 +9,22 @@ export const metadata: Metadata = {
   description: "Created By Owais Abdullah",
 };
 
+const WishlistProvider = dynamic(
+  () => import("./context/WishlistContext").then(mod => mod.WishlistProvider),
+  { ssr: false }
+);
+const NotificationsProvider = dynamic(
+  () => import("./context/NotificationContext").then(mod => mod.NotificationsProvider),
+  { ssr: false }
+);
+const CartProvider = dynamic(
+  () => import("./context/CartContext").then(mod => mod.CartProvider),
+  { ssr: false }
+);
+const ToastContainer = dynamic(
+  () => import("@/components/ui/Toast").then(mod => mod.ToastContainer),
+  { ssr: false }
+);
 const ClerkProvider = dynamic(
   () => import("@/components/sections/ClerkWrapper"),
   { ssr: false }
@@ -25,9 +37,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-        <head>
-        <link rel="preload" href="/globals.css" as="style" />
-      </head>
       <body>
       <ClerkProvider
       afterSignUpUrl="/checkout" // Redirect to checkout after signup
